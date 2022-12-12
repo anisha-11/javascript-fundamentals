0f9e4f254d64c0d1a9c3004ad7f04a95bd467b1d
Anisha Hirani <anisha.hirani11@gmail.com>
2022-12-12 18:54:01 +0000

    ex 1 complete

---
+++ phase-1/09_arrays/name.js
@@ -0,0 +1,8 @@
+const names = ['Anisha', 'Mother', 'Father', 'Sister1', 'Sister2', 'Sister3'];
+
+console.log(names)
+console.log(nameslength);
+console.log(names[1]);
+
+const newName = names.concat('Steve');
+console.log(newName)
