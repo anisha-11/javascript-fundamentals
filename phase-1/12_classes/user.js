ea727d20bc68d04bbdef3aa666f9042b4b0da411
Anisha Hirani <anisha.hirani11@gmail.com>
2022-12-13 14:09:02 +0000

    ex1 complete

---
+++ phase-1/12_classes/user.js
@@ -0,0 +1,15 @@
+class User {
+  construuctor(name) {
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getIntroduction() {
+    return `Hi, my name is ${this.name}`;
+  }
+};
+
+module.exports = User;
